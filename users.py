7b555689f4077769e77653ee0a61c84b070a2df4
J'Neil Blough-Swingen <jneil.cottle@gmail.com>
2020-11-11 18:44:33 -0800

    added functions to idenpendently add rates and books

--- users.py
+++ users.py
@@ -8,7 +8,7 @@ class User:
         self.books = [] #list of isbns
         self.rates = {} #dict with isbn:rate
 
-    def getuser(self, id):
+    def getUser(self, id):
         self.id = id
         #connect to database
         conn = sqlite3.connect("bookreviews.db")
@@ -42,10 +42,12 @@ class User:
 
         cursor.execute('SELECT MAX(user.id) FROM user')
         new_id = cursor.fetchone()[0]+1
+        self.id = new_id
 
         #add user to user table
         if age is not None:
             cursor.execute('INSERT INTO user (id, age) VALUES (?, ?)', (new_id, age))
+            self.age = age
         else:
             cursor.execute('INSERT INTO user (id, age) VALUES (?, 0)', (new_id,))
         print('New User created; id = {}'.format(new_id))
@@ -53,22 +55,25 @@ class User:
 
         #add any books read to reviewImp
         if books:
-            #loop through books (isbns)
             for book in books:
-                cursor.execute('INSERT INTO reviewImp (isbn, user_id, rate) VALUES (?, ?, ?)', (book, new_id, 1))
-
+                #add book to db
+                cursor.execute('INSERT INTO reviewImp (isbn, user_id, rate) VALUES (?, ?, ?)', (book, self.id, 1))
+                #add books to object
+                self.books.append(book)
         #add any ratings to reviewExp
         if rates:
-            #loop through books
             for i, book in enumerate(rates):
-                cursor.execute('INSERT INTO reviewExp (isbn, user_id, rate) VALUES (?, ?, ?)', (book, new_id, rates[book]))
+                #add rates to db
+                cursor.execute('INSERT INTO reviewExp (isbn, user_id, rate) VALUES (?, ?, ?)', (book, self.id, rates[book]))
+                #add rates to object
+                self.rates[book] = rates[book]
 
         conn.commit()
         conn.close()
+        return self
 
-
-
-    def deleteUser(self, id):
+    def deleteUser(self):
+        id = self.id
         conn = sqlite3.connect("bookreviews.db")
         cursor = conn.cursor()
         cursor.execute('DELETE FROM user WHERE user.id = ?', (id,))
@@ -78,18 +83,52 @@ class User:
         print('User {} deleted'.format(id))
         conn.close()
 
+    def deleteUser_id(self, id):
+        conn = sqlite3.connect("bookreviews.db")
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM user WHERE user.id = ?', (id,))
+        cursor.execute('DELETE FROM reviewImp WHERE reviewImp.user_id = ?', (id,))
+        cursor.execute('DELETE FROM reviewExp WHERE reviewExp.user_id = ?', (id,))
+        conn.commit()
+        print('User {} deleted'.format(id))
+        conn.close()
 
-#add a book to list of books
-#add book/rate to rates
+    def addBooks(self, books):
+        conn = sqlite3.connect("bookreviews.db")
+        cursor = conn.cursor()
+        for book in books:
+            #add book to db
+            cursor.execute('INSERT INTO reviewImp (isbn, user_id, rate) VALUES (?, ?, ?)', (book, self.id, 1))
+            #add books to object
+            self.books.append(book)
+        conn.commit()
+        conn.close()
+
+    def addRates(self, rates):
+        conn = sqlite3.connect("bookreviews.db")
+        cursor = conn.cursor()
+
+        for i, book in enumerate(rates):
+            #add rates to db
+            cursor.execute('INSERT INTO reviewExp (isbn, user_id, rate) VALUES (?, ?, ?)', (book, self.id, rates[book]))
+            #add rates to object
+            self.rates[book] = rates[book]
+
+        conn.commit()
+        conn.close()
+
+
 #recommend books for this user ** big one! will need to call functions for actual recommender
 
 
 if __name__ == '__main__':
 
     u = User()
-    u.getuser(2)
-    print(u.books)
-    u.getuser(276726)
-    print(u.rates)
-    u.makeUser(rates = {'0195153448':3, '0155061224':6}, books = ['0195153448', '0155061224'], age= 34)
-    u.deleteUser(278855)
+    u1 = u.makeUser(age= 34, rates = {'0195153448':3})
+    print(u1.rates)
+    u1.addBooks(books = ['0195153448', '0155061224'])
+    u1.addRates(rates =  {'0155061224':6})
+    print(u1.rates)
+    print(u1.books)
+
+    u1.deleteUser()
