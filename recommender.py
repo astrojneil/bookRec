307ebe4623fe21e8cd314e17fc720aa1eab8c049
J'Neil Blough-Swingen <jneil.cottle@gmail.com>
2020-12-02 20:01:41 -0800

    reduce size of pivot table

--- recommender.py
+++ recommender.py
@@ -9,9 +9,35 @@ from users import User
 from books import Book
 
 #function to retrieve reviews from sql table
-def getReviewTable():
+def getReviewTable(rateType, user):
     conn = sqlite3.connect("bookreviews.db")
-    reviews = pd.read_sql('SELECT isbn, user_id, rate FROM reviewExp', conn)
+
+    if rateType == 'exp':
+        execute_string = 'SELECT isbn, user_id, rate FROM reviewExp WHERE ( '
+        booklist = []
+        for i, isbn in enumerate(user.rates):
+            if i == 0:
+                execute_string = execute_string+'isbn = ? '
+            else:
+                execute_string = execute_string+'OR isbn = ? '
+            booklist.append(isbn)
+        execute_string = execute_string+')'
+
+        reviews = pd.read_sql(execute_string, conn, params=booklist)
+        #reviews = pd.read_sql('SELECT isbn, user_id, rate FROM reviewExp', conn)
+    else:
+        execute_string = 'SELECT isbn, user_id, rate FROM reviewImp WHERE ( '
+        booklist = []
+        for i, isbn in enumerate(user.books):
+            if i == 0:
+                execute_string = execute_string+'isbn = ? '
+            else:
+                execute_string = execute_string+'OR isbn = ? '
+            booklist.append(isbn)
+        execute_string = execute_string+')'
+
+        #reviews = pd.read_sql(execute_string, conn, params=booklist)
+        reviews = pd.read_sql('SELECT isbn, user_id, rate FROM reviewImp', conn)
     conn.close()
     return reviews
 
@@ -77,9 +103,16 @@ def findBooks(user_inds, ratings_matrix):
 
 #find the most likely to be read over all books
 def recommendbook(user):
+    rateType = 'imp'
+    if rateType != 'imp' and rateType != 'exp':
+        print('Unknown rating type!')
+        return
+
     #read in review table
-    ratings_matrix = makeMatrix(getReviewTable())
-    rateType = 'exp'
+    ratings_matrix = makeMatrix(getReviewTable(rateType, user))
+
+    user_loc = ratings_matrix.index.get_loc(user.id)
+    user_books = user.rates #dictionary of books created; turn this into a vector by looking up isbns in matrix
 
     if rateType != 'imp' and rateType != 'exp':
         print('Unknown rating type!')
@@ -88,8 +121,6 @@ def recommendbook(user):
     knn_model = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
     knn_model.fit(ratings_matrix)
 
-    user_loc = ratings_matrix.index.get_loc(user.id)
-    user_books = user.rates #dictionary of books created; turn this into a vector by looking up isbns in matrix
     #change this to use user functions to get user id and list of ratings
     #so that this can work with a user that's not already in the table?
     user_vec = ratings_matrix.iloc[user_loc, :].values.reshape(1, -1)
@@ -100,7 +131,6 @@ def recommendbook(user):
     #ignore the first item, it is the original user
     sims = 1-dist.flatten()[1:] #most similar is closest to 1
     sim_user_ind = indices.flatten()[1:]
-    print(sims)
 
     #list books similar users have read
     simUserBooks, bookValues = findBooks(sim_user_ind, ratings_matrix)
